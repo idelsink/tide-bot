b41cd51cea1e6dc70f41f283e9327219ec861ce2
Ingmar Delsink <ingmardelsink@hotmail.com>
2019-07-09 21:02:12 +0200

    Add beep and server command

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,6 @@
 module.exports = {
   name: 'ping',
-  aliases: ['foo', 'bar'],
   description: 'Ping!',
-  args: false,
-  usage: 'some test',
-  guildOnly: true,
-  cooldown: 1,
   execute (message, args) {
     message.channel.send('Pong.');
   }
