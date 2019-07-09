5a77f79a51533229ef76731301bf2f469739ae95
Ingmar Delsink <ingmardelsink@hotmail.com>
2019-07-09 20:59:56 +0200

    Add arg check, command usage, guild only commands, cooldowns, aliasses and help command

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const client = new Discord.Client();
+
+const cooldowns = new Discord.Collection();
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -23,12 +25,47 @@ client.on('message', message => {
   if (!message.content.match(new RegExp(`^<@${client.user.id}>`))) return; // Does not start with a mention
 
   const args = message.content.slice(`<@${client.user.id}>`.length).match(/\S+/g) || [];
-  const command = _.invoke(_.invoke(args, 'shift'), 'toLowerCase');
+  const commandName = _.invoke(_.invoke(args, 'shift'), 'toLowerCase');
 
-  if (!client.commands.has(command)) return; // Does not have the command
+  const command = client.commands.get(commandName) ||
+    client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+  if (!command) return;
+
+  // Cooldown
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Discord.Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000;
+
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      return; // Just silently return, ignore the spam
+    }
+  }
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+  if (command.guildOnly && message.channel.type !== 'text') {
+    return message.reply('I can\'t execute that command inside DMs!');
+  }
+
+  if (command.args && !args.length) {
+    let reply = `You didn't provide any arguments, ${message.author}!`;
+
+    if (command.usage) {
+      reply += `\nThe proper usage would be: <@${client.user.id}> \`${command.name} ${command.usage}\``;
+    }
+
+    return message.channel.send(reply);
+  }
 
   try {
-    client.commands.get(command).execute(message, args);
+    command.execute(message, args);
   } catch (error) {
     console.error(error);
     message.reply('there was an error trying to execute that command!');
