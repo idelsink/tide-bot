699e030f6bd26c226c9c8ef5d8f80812f26dae72
Ingmar Delsink <ingmardelsink@hotmail.com>
2019-07-10 22:57:49 +0200

    Remove '.' in replay after '!'

--- commands/beep.js
+++ commands/beep.js
@@ -2,6 +2,6 @@ module.exports = {
   name: 'beep',
   description: 'Beep!',
   execute (message) {
-    message.channel.send('all the boops!.');
+    message.channel.send('all the boops!');
   }
 };
