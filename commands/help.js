b0c6da4c648eb9d07d01d1f22ed89fb0b1b21caf
Ingmar Delsink <ingmardelsink@hotmail.com>
2019-07-10 22:58:19 +0200

    Remove reply of dm message was send

--- commands/help.js
+++ commands/help.js
@@ -17,7 +17,7 @@ module.exports = {
       return message.author.send(data, { split: true })
         .then(() => {
           if (message.channel.type === 'dm') return;
-          message.reply('I\'ve sent you a DM with all my commands!');
+          // message.reply('I\'ve sent you a DM with all my commands!');
         })
         .catch(error => {
           console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
